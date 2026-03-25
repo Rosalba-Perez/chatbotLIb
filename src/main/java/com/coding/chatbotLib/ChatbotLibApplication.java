719c28632751ced0567d04e8a94eaf36a0f0fda3
Rosalba Perez <rous092006@gmail.com>
2026-03-25 16:15:53 -0500

    creación proyecto chatbotLIb

---
+++ src/main/java/com/coding/chatbotLib/ChatbotLibApplication.java
@@ -0,0 +1,13 @@
+package com.coding.chatbotLib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatbotLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatbotLibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coding/chatbotLib/ChatbotLibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coding.chatbotLib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbotLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
